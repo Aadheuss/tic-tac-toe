ce31e420ef2b453d264ecc9752693145089a262a
Aadheuss <aadheusx@gmail.com>
2022-10-12 20:01:47 +0700

    Add function to keep score and show win
    i add multiple functions that will input the winner of each round and
    output it to html. if a player has play for three round show the winning
    player.

--- js/javascript.js
+++ js/javascript.js
@@ -4,11 +4,14 @@ const Lobby = (() => {
   const startButton = document.querySelector('.start');
   const game = document.querySelector('.game');
   const playerContainer = document.querySelector('.player-container');
+  const winnerExitBtn = document.querySelector('.winner > button');
+  winnerExitBtn.addEventListener('click', toggleHidden.bind(winnerExitBtn, winnerExitBtn.parentElement));
 
   function checkState () { 
     if (Players.length === 2) {
       startButton.classList.remove('hidden');
       startButton.addEventListener('click', startGame);
+      winCon.startTurn();
     }
   }
 
@@ -27,12 +30,13 @@ const Lobby = (() => {
   }
   
   function toggleHidden (value) {
-    value.classList.toggle('hidden');
+    value.classList.toggle('hidden')
   }
   
   return {Players, checkState};
 })()
  
+
 const Players = (() => {
   const player = (id, type, weapon) => {
     return {id, type, weapon};
@@ -96,7 +100,6 @@ const Players = (() => {
 const Game = (() => {
   const domElements = document.querySelector('.gameboard');
   const gameBoard = [];
-  let turn = 0;
 
   function tiles (index, value) {
     return {index, value};
@@ -108,29 +111,7 @@ const Game = (() => {
   }
   
   gameBoard.forEach(tiles => render.call(tiles, domElements, 'div'));
-
-  document.querySelectorAll('.gameboard > div').forEach(tiles => tiles.addEventListener('click', round))
-
-  function round () {
-      if (turn % 2 === 0) {
-        playerTurn.call(this, 0);
-      } else {
-        playerTurn.call(this, 1);
-      }
-  }
-
-  function playerTurn (i) {
-    if (this.textContent === '') {
-    this.value = Lobby.Players[i].weapon;
-    this.textContent = this.value;
-    const index = Number(this.getAttribute('data-index'));
-    gameBoard[index].value = this.value;
-    turn++;
-    }
-
-    winCon.gameLogic();
-  }
-
+ 
   function render (container, type) {
     const item = document.createElement(type);
     item.setAttribute('data-index', `${this.index}`);
@@ -140,19 +121,88 @@ const Game = (() => {
   return {gameBoard};
 })();
 
+
 const winCon = (() => {
   const gameBoard = Game.gameBoard;
+  const gameBoardDom = document.querySelectorAll('.gameboard > div');
+  let turn = 1;
+  let win = false;
+
+  gameBoardDom.forEach(tiles => tiles.addEventListener('click', startRound));
+ 
+  const scoreBoard = (() => {
+    let round = 1;
+    let p1Score = 0;
+    let p2Score = 0;
+    return {p1Score, p2Score, round};
+  })();
+  
+  function startRound () {
+    if (turn % 2 === 0) {
+      playerTurn.call(this, 1);
+    } else {
+      playerTurn.call(this, 0);
+    }
+  }
+
+  function playerTurn (i) {
+    if (this.textContent === '') {
+    this.value = Lobby.Players[i].weapon;
+    this.textContent = this.value;
+    const index = Number(this.getAttribute('data-index'));
+    gameBoard[index].value = this.value;
+    turn++;
+    gameLogic();
+    updateTurn();
+    resetBoard();
+    }
+  }
+
+  function updateTurn () {
+    const p1ScoreDom = document.querySelector('.scoreboard > div:first-child');
+    const p2ScoreDom = document.querySelector('.scoreboard > div:last-child');
+    p1ScoreDom.classList.toggle('turn');
+    p2ScoreDom.classList.toggle('turn');
+  }
+
+  function startTurn () {
+    const p1ScoreDom = document.querySelector('.scoreboard > div:first-child');
+    const p2ScoreDom = document.querySelector('.scoreboard > div:last-child');
+
+    if (Lobby.Players[0].id === 'player1') {
+    p1ScoreDom.classList.add('turn');
+    p2ScoreDom.classList.remove('turn')
+    } else {
+    p2ScoreDom.classList.add('turn');
+    p1ScoreDom.classList.remove('turn');
+    }
+  }
+
+  function checkWinner (winner) {
+    if (winner.id === 'player1') {
+      scoreBoard.p1Score += 1;
+      updateScore()
+    } else if (winner.id === 'player2') {
+      scoreBoard.p2Score += 1;
+      updateScore()
+    } else {
+      updateScore();
+    }
+
+    updateRound();
+    resetBoard();
+
+    if (scoreBoard.round > 3) {
+      announceWinner();
+    }
+  }
+
   function gameLogic () {
-    checkRow ([0, 3, 6]);
-    checkColumn([0, 1, 2]);
-    checkCross(4);
     function checkRow (array) {
       array.forEach(a => {
         const b = a + 1;
         const c = b + 1;
-        if (gameBoard[a].value !==  '' && gameBoard[a].value === gameBoard[b].value && gameBoard[b].value === gameBoard[c].value) {
-          console.log(gameBoard[a].value);
-        }
+        checkValue(a, b, c);
       })
     }
 
@@ -160,26 +210,104 @@ const winCon = (() => {
       array.forEach(a => {
         const b = a + 3;
         const c = b + 3;
-        if (gameBoard[a].value !==  '' && gameBoard[a].value === gameBoard[b].value && gameBoard[b].value === gameBoard[c].value) {
-          console.log(gameBoard[a].value);
-        }
+        checkValue(a, b, c);
       })
     }
     
-    function checkCross (n) {
-      const a = n + 2;
-      const b = n - 2;
-      const c = n + 4;
-      const d = n - 4;
-      if (gameBoard[n].value !==  '' && gameBoard[n].value === gameBoard[a].value && gameBoard[a].value === gameBoard[b].value ||
-          gameBoard[n].value !==  '' && gameBoard[n].value === gameBoard[c].value && gameBoard[c].value === gameBoard[d].value  ) {
-        console.log(gameBoard[n].value);
+    function checkCross (array) {
+      array.forEach(n => {
+        const a = 4;
+        const b = a + n;
+        const c = a - n;
+        checkValue(a, b, c);
+      })
+    }
+    
+    function checkValue (a, b, c) {
+      if (gameBoard[a].value !==  '' && gameBoard[a].value === gameBoard[b].value && gameBoard[b].value === gameBoard[c].value) {
+        processValue(a, b, c);
+      } else {
+        win = false;
       }
+    }
+
+    function processValue (a, b, c) {
+
+      const winnerArray = [gameBoard[a].index, gameBoard[b].index, gameBoard[c].index];
+      winnerArray.forEach(item => {
+        const winnerItem = document.querySelector(`[data-index = '${item}']`)
+        winnerItem.classList.add('win')
+      })
+
+      const result = Lobby.Players.find(item => item.weapon === gameBoard[a].value);
+      win = true;
+      checkWinner(result);
+    }
+
+    checkRow ([0, 3, 6]);
+    checkColumn([0, 1, 2]);
+    checkCross([4, 2]);
+  }
+
+  function updateScore () {
+    const p1ScoreDom = document.querySelector('.scoreboard > div:first-child > span');
+    const p2ScoreDom = document.querySelector('.scoreboard > div:last-child > span');
+    p1ScoreDom.textContent = scoreBoard.p1Score;
+    p2ScoreDom.textContent = scoreBoard.p2Score;
+  }
+
+  function updateRound () {
+    const roundDom = document.querySelector('.round > span');
+    scoreBoard.round += 1;
+    if (scoreBoard.round <=3) {
+      roundDom.textContent = scoreBoard.round;
     }
   }
 
-  return {gameLogic};
-})()
+  function resetBoard () {
+    const full = gameBoard.every(tiles => tiles.value !== '');
 
-console.log(winCon.winner);
+    if (win === true || full === true) {
+      gameBoardDom.forEach(tiles => tiles.removeEventListener('click', startRound));
+      gameBoard.forEach(item => item.value = '');
+      if (full === true) {
+        checkWinner('none');
+        gameBoardDom.forEach(item => item.classList.add('tie'));
+      }
+      setTimeout(() => {
+        gameBoardDom.forEach(tiles => {
+          tiles.textContent = '';
+          tiles.classList.remove('win', 'tie');
+        });
+        if (scoreBoard.round <= 3) {
+          gameBoardDom.forEach(tiles => tiles.addEventListener('click', startRound));
+        }
+      }, 1500)
+    }
+  }
+
+  function announceWinner () {
+    const winner = document.querySelector('.winner > div');
+    winner.parentElement.classList.toggle('hidden');
+    
+    if (scoreBoard.p1Score > scoreBoard.p2Score) {
+      winner.textContent = 'Player1 wins!'
+    } else if (scoreBoard.p1Score < scoreBoard.p2Score) {
+      winner.textContent = 'Player2 wins!';
+    } else {
+      winner.textContent = `It's a tie!`;
+    }
+    
+    document.querySelector('.restart').classList.toggle('hidden');
+    
+    function playAgain () {
+    scoreBoard.round = 0;
+    scoreBoard.p1Score = 0;
+    scoreBoard.p2Score = 0;
+    updateScore();
+    updateRound();
+    }
+  }
+  return {startRound, startTurn};
+})()
 //a function inside gameboard will keep track of the value of the array;
